144240a679200a935f7a0fedaa0b0be071d2a49a
BlueProg <blueprog42@gmail.com>
2017-03-29 17:45:24 +0200

    Base fonction

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+var datas = [
+	{ 'info': 'exemple 1' },
+	{ 'info': 'autre' }
+];
+
+app.use(function (req, res, next) {
+      console.log(req.method, req.url, req.body);
+      next();
+  });
+
+app.route('/token')
+	.get(function(req, res) {
+		res.send('token');
+	})
+
+app.route('/datas')
+	.get(function(req, res) {
+		res.send(datas);
+	})
+	.post(function(req, res) {
+		var data = req.body.data;
+		datas.push({'info': data.info});
+		res.send(datas);
+	})
+	.delete(function(req, res) {
+		datas = [];
+		res.send(datas);
+	})
 
 console.log("Serveur lancé au port 3000")
 app.listen(3000);
--- public/app.js
+++ public/app.js
@@ -1,5 +1,43 @@
 var app = angular.module('app', [])
 
-.controller('AppController', function ($scope) {
+.controller('AppController', function ($scope, ServiceApi) {
 	$scope.title = 'JSON Web Token';
+	$scope.newData = '';
+
+	$scope.getServerData = function() {
+		ServiceApi.getData().then(function (datas) {
+			$scope.datas = datas.data;
+		});
+	}
+	
+	$scope.getServerData();
+
+	$scope.addData = function() {
+		ServiceApi.postData($scope.newData).then(function (datas) {
+			$scope.datas = datas.data;
+			$scope.newData = '';
+		});
+	}
+
+	$scope.deleteData = function() {
+		ServiceApi.deleteData().then(function (datas) {
+			$scope.datas = datas.data;
+		});
+	}
 })
+
+.factory('ServiceApi', ['$http', function($http) {
+
+	var api = {};
+
+	api.getData = function() {
+		return $http.get('/datas');
+	}
+	api.postData = function(data) {
+		return $http.post('/datas', { 'data' : {'info' : data}});
+	}
+	api.deleteData = function(data) {
+		return $http.delete('/datas');
+	}
+	return api;
+}])
